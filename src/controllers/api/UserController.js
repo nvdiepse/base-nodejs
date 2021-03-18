fe4e02c151d2cee4664eca692fdd685cd232129d
Diepnv2 <diepnv2@vnext.com.vn>
2021-03-18 17:34:05 +0700

    [Update] defaul function

--- src/controllers/api/UserController.js
+++ src/controllers/api/UserController.js
@@ -1,8 +1,18 @@
-const logger = require("../../libs/logger");
-const getUsers = (req, res) => {
-    console.log(req.originalUrl);
+const knex = require('../../config/db');
+const {HOME} = require('../../constants/common');
+const getUsers = async (req, res) => {
+    try {
+        console.log(HOME)
+        // let data = await knex.select('*').from('users');
+        // data.map(item => {
+        //     item.__link = item.id;
+        //     return item;
+        // })
+        // console.log(data);
+    } catch (e) {
+        console.log(e);
+    }
 }
-
 
 module.exports = {
     getUsers
